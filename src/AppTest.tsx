5eec5bce64580daae3e2e6788be75ef040439510
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2024-03-28 13:08:04 -0300

    Update CSS imports and fix Divider orientation

--- src/AppTest.tsx
+++ src/AppTest.tsx
@@ -2,7 +2,7 @@ import { Divider } from "./components/Divider";
 
 function AppTest() {
   return (
-    <div className=" w-1/2 h-12 mx-auto flex justify-center items-center bg-slate-50 mt-12">
+    <div className=" w-1/2 h-12 mx-auto flex justify-center items-center bg-slate-200 mt-12 br-diver">
       {/* <div className="flex flex-row justify-around items-center gap-4 w-1/2 mx-auto mt-12 bg-slate-100">
         <div className="bg-slate-800 text-lime-200 text-3xl text-center">
           Exemplo
@@ -36,7 +36,7 @@ function AppTest() {
         </div>
       </div> */}
 
-      <Divider orientation="vertical" size="lg" dashed={false} />
+      <Divider orientation="horizontal" size="lg" dashed={false} className="" />
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppTest from "./AppTest.tsx";
-import "./index.css";
+import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
