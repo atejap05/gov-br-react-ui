43d9307f8a5ceae3b003b09c5f4014aeaf6740ea
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2024-04-01 15:31:06 -0300

    testes de card story

--- src/AppTest.tsx
+++ src/AppTest.tsx
@@ -3,18 +3,18 @@ import { Card, CardContent, CardHeader, CardFooter } from "./components/Card";
 function AppTest() {
   return (
     <div className="flex place-items-center place-content-center h-screen w-full">
-      <Card hover fixed>
+      <Card>
         <CardHeader>
-          <h1 className="text-2xl font-bold">Card Header</h1>
+          <p className="text-xl">Card Header</p>
         </CardHeader>
+
         <CardContent>
-          <h1 className="text-2xl font-bold">Hello World</h1>
-          <p className="text-gray-600">This is a test</p>
-          <h1 className="text-2xl font-bold">Hello World</h1>
+          <p className="text-lg font-bold">Hello World</p>
           <p className="text-gray-600">This is a test</p>
         </CardContent>
+
         <CardFooter>
-          <p className="text-2xl font-bold">Card Footer</p>
+          <p className="text-sm font-bold">Card Footer</p>
         </CardFooter>
       </Card>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ["./src/**/*.{html,js,jsx,ts,tsx,mdx}"];
-export const theme = {
-  extend: {
-    colors: {
-      teste01: "#FF6363",
-      teste02: {
-        100: "#E2E2D5",
-        200: "#888883",
+
+export default {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx,mdx}"],
+  theme: {
+    extend: {
+      colors: {
+        teste01: "#FF6363",
+        teste02: {
+          100: "#E2E2D5",
+          200: "#888883",
+        },
       },
     },
   },
+  plugins: [],
 };
-export const plugins = [];
