76a9fdeb6cd6715b238c0fbbda18f1b16dc258a7
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2024-04-01 11:41:39 -0300

    Add Card component to AppTest

--- src/AppTest.tsx
+++ src/AppTest.tsx
@@ -1,8 +1,10 @@
+import { Card } from "./components/Card";
+
 function AppTest() {
   return (
-    <h1 className="text-2xl text-slate-800">
-      Teste na sua branch seu component ou layout
-    </h1>
+    <div className="flex place-items-center place-content-center h-screen w-full">
+      <Card />
+    </div>
   );
 }
 
