7fab897e8b5d1eb4ebbe61a72ab7779f2429f276
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2024-04-03 11:47:33 -0300

    Update chromatic to version 11.3.0

---
+++ storybook-static/assets/Button.stories-CFF6zSlS.js
@@ -0,0 +1,85 @@
+import{B as ur}from"./Button-D9uXYbm8.js";import"./index-Bo_hk2WC.js";import"./index-CsdIBAqE.js";const vr={title:"Components/Button",component:ur,tags:["autodocs"],parameters:{layout:"centered"},argTypes:{variant:{control:{type:"radio",options:["primary","secondary","terciary","danger","success","warning","info"]}},size:{control:{type:"radio",options:["xs","sm","md","lg"]}},circle:{control:{type:"boolean"}},disabled:{control:{type:"boolean"}},active:{control:{type:"boolean"}},loading:{control:{type:"boolean"}},dark:{control:{type:"boolean"}},block:{control:{type:"boolean"}}}},r={args:{children:"Button"}},e={args:{variant:"primary",children:"Primary"}},a={args:{variant:"secondary",children:"Secondary"}},s={args:{variant:"tertiary",children:"Terciary"}},n={args:{variant:"danger",children:"Danger"}},o={args:{variant:"success",children:"Success"}},c={args:{variant:"warning",children:"Warning"}},t={args:{variant:"info",children:"Info"}},i={args:{size:"xs",children:"Extra Small"}},d={args:{size:"sm",children:"Small"}},l={args:{size:"lg",children:"Large"}},m={args:{circle:!0,children:"🔍"}},p={args:{disabled:!0,children:"Disabled"}},g={args:{active:!0,children:"Active"}},u={args:{loading:!0,children:"Loading"}},h={args:{dark:!0,children:"Dark Mode"}},S={args:{block:!0,children:"Block"}};var y,v,D;r.parameters={...r.parameters,docs:{...(y=r.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    children: "Button"
+  }
+}`,...(D=(v=r.parameters)==null?void 0:v.docs)==null?void 0:D.source}}};var b,k,f;e.parameters={...e.parameters,docs:{...(b=e.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    variant: "primary",
+    children: "Primary"
+  }
+}`,...(f=(k=e.parameters)==null?void 0:k.docs)==null?void 0:f.source}}};var x,B,L;a.parameters={...a.parameters,docs:{...(x=a.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    variant: "secondary",
+    children: "Secondary"
+  }
+}`,...(L=(B=a.parameters)==null?void 0:B.docs)==null?void 0:L.source}}};var z,E,T;s.parameters={...s.parameters,docs:{...(z=s.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  args: {
+    variant: "tertiary",
+    children: "Terciary"
+  }
+}`,...(T=(E=s.parameters)==null?void 0:E.docs)==null?void 0:T.source}}};var A,I,M;n.parameters={...n.parameters,docs:{...(A=n.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    variant: "danger",
+    children: "Danger"
+  }
+}`,...(M=(I=n.parameters)==null?void 0:I.docs)==null?void 0:M.source}}};var P,W,w;o.parameters={...o.parameters,docs:{...(P=o.parameters)==null?void 0:P.docs,source:{originalSource:`{
+  args: {
+    variant: "success",
+    children: "Success"
+  }
+}`,...(w=(W=o.parameters)==null?void 0:W.docs)==null?void 0:w.source}}};var C,_,O;c.parameters={...c.parameters,docs:{...(C=c.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    variant: "warning",
+    children: "Warning"
+  }
+}`,...(O=(_=c.parameters)==null?void 0:_.docs)==null?void 0:O.source}}};var j,q,F;t.parameters={...t.parameters,docs:{...(j=t.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  args: {
+    variant: "info",
+    children: "Info"
+  }
+}`,...(F=(q=t.parameters)==null?void 0:q.docs)==null?void 0:F.source}}};var G,H,J;i.parameters={...i.parameters,docs:{...(G=i.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  args: {
+    size: "xs",
+    children: "Extra Small"
+  }
+}`,...(J=(H=i.parameters)==null?void 0:H.docs)==null?void 0:J.source}}};var K,N,Q;d.parameters={...d.parameters,docs:{...(K=d.parameters)==null?void 0:K.docs,source:{originalSource:`{
+  args: {
+    size: "sm",
+    children: "Small"
+  }
+}`,...(Q=(N=d.parameters)==null?void 0:N.docs)==null?void 0:Q.source}}};var R,U,V;l.parameters={...l.parameters,docs:{...(R=l.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    size: "lg",
+    children: "Large"
+  }
+}`,...(V=(U=l.parameters)==null?void 0:U.docs)==null?void 0:V.source}}};var X,Y,Z;m.parameters={...m.parameters,docs:{...(X=m.parameters)==null?void 0:X.docs,source:{originalSource:`{
+  args: {
+    circle: true,
+    children: "🔍"
+  }
+}`,...(Z=(Y=m.parameters)==null?void 0:Y.docs)==null?void 0:Z.source}}};var $,rr,er;p.parameters={...p.parameters,docs:{...($=p.parameters)==null?void 0:$.docs,source:{originalSource:`{
+  args: {
+    disabled: true,
+    children: "Disabled"
+  }
+}`,...(er=(rr=p.parameters)==null?void 0:rr.docs)==null?void 0:er.source}}};var ar,sr,nr;g.parameters={...g.parameters,docs:{...(ar=g.parameters)==null?void 0:ar.docs,source:{originalSource:`{
+  args: {
+    active: true,
+    children: "Active"
+  }
+}`,...(nr=(sr=g.parameters)==null?void 0:sr.docs)==null?void 0:nr.source}}};var or,cr,tr;u.parameters={...u.parameters,docs:{...(or=u.parameters)==null?void 0:or.docs,source:{originalSource:`{
+  args: {
+    loading: true,
+    children: "Loading"
+  }
+}`,...(tr=(cr=u.parameters)==null?void 0:cr.docs)==null?void 0:tr.source}}};var ir,dr,lr;h.parameters={...h.parameters,docs:{...(ir=h.parameters)==null?void 0:ir.docs,source:{originalSource:`{
+  args: {
+    dark: true,
+    children: "Dark Mode"
+  }
+}`,...(lr=(dr=h.parameters)==null?void 0:dr.docs)==null?void 0:lr.source}}};var mr,pr,gr;S.parameters={...S.parameters,docs:{...(mr=S.parameters)==null?void 0:mr.docs,source:{originalSource:`{
+  args: {
+    block: true,
+    children: "Block"
+  }
+}`,...(gr=(pr=S.parameters)==null?void 0:pr.docs)==null?void 0:gr.source}}};const Dr=["Default","Primary","Secondary","Tertiary","Danger","Success","Warning","Info","ExtraSmall","Small","Large","Circle","Disabled","Active","Loading","DarkMode","Block"];export{g as Active,S as Block,m as Circle,n as Danger,h as DarkMode,r as Default,p as Disabled,i as ExtraSmall,t as Info,l as Large,u as Loading,e as Primary,a as Secondary,d as Small,o as Success,s as Tertiary,c as Warning,Dr as __namedExportsOrder,vr as default};
---
+++ storybook-static/assets/divider.stories-D2aXNxT4.js
@@ -0,0 +1,53 @@
+import{j as e}from"./index-Bo_hk2WC.js";import{D as n}from"./Divider-BmKS9bOJ.js";import"./index-CsdIBAqE.js";const u={title:"Exemplos/Divider",component:n,parameters:{layout:"centered"},argTypes:{orientation:{control:!1},size:{control:!1},dashed:{control:!1}}},r={render:()=>e.jsxs("div",{style:{display:"flex",flexDirection:"column",justifyContent:"space-between",alignItems:"center",gap:"1rem"},children:[e.jsx("h1",{children:"Conteúdo 1"}),e.jsx(n,{}),e.jsx("h3",{children:"Conteúdo 2"})]})},t={render:()=>e.jsxs("div",{style:{display:"flex",justifyContent:"space-between",alignItems:"center",gap:"1.6rem"},children:[e.jsx("h3",{children:"Conteúdo 1"}),e.jsx(n,{orientation:"vertical"}),e.jsx("h3",{children:"Conteúdo 2"})]})},o={render:()=>e.jsxs("div",{style:{display:"flex",flexDirection:"column",gap:"2rem"},children:[e.jsxs("div",{style:{display:"flex",justifyContent:"center",alignItems:"center",gap:"1rem"},children:[e.jsx("p",{style:{marginTop:"1rem"},children:"Conteúdo 1"}),e.jsx(n,{orientation:"vertical"}),e.jsx("p",{style:{marginTop:"1rem"},children:"Conteúdo 2"})]}),e.jsx(n,{size:"lg",dashed:!0}),e.jsx("div",{style:{textAlign:"center"},children:e.jsx("h1",{children:"Conteúdo 3"})})]})};var s,i,a;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: () => <div style={{
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: "1rem"
+  }}>\r
+      <h1>Conteúdo 1</h1>\r
+\r
+      <Divider />\r
+\r
+      <h3>Conteúdo 2</h3>\r
+    </div>
+}`,...(a=(i=r.parameters)==null?void 0:i.docs)==null?void 0:a.source}}};var d,l,c;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: () => <div style={{
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: "1.6rem"
+  }}>\r
+      <h3>Conteúdo 1</h3>\r
+      <Divider orientation="vertical" />\r
+      <h3>Conteúdo 2</h3>\r
+    </div>
+}`,...(c=(l=t.parameters)==null?void 0:l.docs)==null?void 0:c.source}}};var p,m,x;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: () => <div style={{
+    display: "flex",
+    flexDirection: "column",
+    gap: "2rem"
+  }}>\r
+      <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      gap: "1rem"
+    }}>\r
+        <p style={{
+        marginTop: "1rem"
+      }}>Conteúdo 1</p>\r
+        <Divider orientation="vertical" />\r
+        <p style={{
+        marginTop: "1rem"
+      }}>Conteúdo 2</p>\r
+      </div>\r
+      <Divider size="lg" dashed />\r
+      <div style={{
+      textAlign: "center"
+    }}>\r
+        <h1>Conteúdo 3</h1>\r
+      </div>\r
+    </div>
+}`,...(x=(m=o.parameters)==null?void 0:m.docs)==null?void 0:x.source}}};const v=["Horizontal","Vertical","Compounded"];export{o as Compounded,r as Horizontal,t as Vertical,v as __namedExportsOrder,u as default};
