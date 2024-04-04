9a32025b6758e48cd94a6019f873f62049d69b48
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2024-04-04 14:50:00 -0300

    Update storybook static assets

---
+++ storybook-static/assets/WithTooltip-Y7J54OF7-CFJFAskV.js
@@ -0,0 +1,1 @@
+import{W as e,W as h,b as l}from"./DocsRenderer-K4EAMTCU-BsunTNiV.js";import"./iframe-xJYHWf1b.js";import"../sb-preview/runtime.js";import"./index-CsdIBAqE.js";import"./react-18-Cu1enTKq.js";import"./index-B_J8iUie.js";import"./_getPrototype-DcQZ1DAh.js";import"./index-DrFu-skq.js";export{e as WithToolTipState,h as WithTooltip,l as WithTooltipPure};
---
+++ storybook-static/assets/card.stories-BfkZiH2r.js
@@ -0,0 +1,19 @@
+import{j as e}from"./index-Bo_hk2WC.js";import{C as t,b as s}from"./CardFooter-bDqatcva.js";import"./index-CsdIBAqE.js";const i={title:"Exemplos/Card",component:t,parameters:{layout:"centered"}},r={render:()=>e.jsxs(e.Fragment,{children:[e.jsx("h1",{children:"Card simples e completo"}),e.jsxs("div",{className:"flex justify-between items-center gap-4",children:[e.jsx(t,{children:e.jsx(s,{children:e.jsx("img",{src:"https://picsum.photos/id/0/500",alt:"Imagem de exemplo"})})}),e.jsx(t,{children:e.jsx(s,{children:e.jsx("img",{src:"https://picsum.photos/id/0/500",alt:"Imagem de exemplo"})})})]})]})};var n,a,m;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: () =>
+  // Card Simples e completo
+  <>\r
+      <h1>Card simples e completo</h1>\r
+      <div className="flex justify-between items-center gap-4">\r
+        <Card>\r
+          <CardContent>\r
+            <img src="https://picsum.photos/id/0/500" alt="Imagem de exemplo" />\r
+          </CardContent>\r
+        </Card>\r
+        <Card>\r
+          <CardContent>\r
+            <img src="https://picsum.photos/id/0/500" alt="Imagem de exemplo" />\r
+          </CardContent>\r
+        </Card>\r
+      </div>\r
+    </>
+}`,...(m=(a=r.parameters)==null?void 0:a.docs)==null?void 0:m.source}}};const c=["Default"];export{r as Default,c as __namedExportsOrder,i as default};
