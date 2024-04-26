e7b6c1b9d8a9456e1a92d1f30b19086c4547ec88
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2024-04-26 17:08:13 -0300

    implementa Radio Group

---
+++ src/components/Radio/RadioGroup.tsx
@@ -0,0 +1,55 @@
+import { cva } from "class-variance-authority";
+import { cn } from "../../utils";
+import { RadioItem, RadioItemProps } from "./RadioItem";
+
+/////// RadioGroup ///////
+
+const radioGroupVariants = cva("", {
+  variants: {
+    orientation: {
+      horizontal: "d-inline-block mr-5 mb-3 text-nowrap",
+      vertical: "mb-3",
+    },
+  },
+});
+
+export type RadioGroupProps = {
+  value?: string;
+  title?: string;
+  subtitle?: string;
+  orientation?: "horizontal" | "vertical";
+  feedback?: string;
+  className?: string;
+  onChoose?: (value: string) => void;
+  options: RadioItemProps[];
+};
+
+export const RadioGroup = ({
+  title,
+  subtitle,
+  feedback,
+  orientation = "vertical",
+  className,
+  options,
+  onChoose,
+}: RadioGroupProps) => {
+  const classes = cn(radioGroupVariants({ orientation }));
+
+  return (
+    <div className={cn("p-2", className)}>
+      {title && <p className="label text-lg mb-0">{title}</p>}
+      {subtitle && <p className="help-text text-sm">{subtitle}</p>}
+      {options.map(option => (
+        <div key={option.id} className={classes}>
+          <RadioItem
+            {...option}
+            onChange={e => {
+              onChoose && onChoose(e.target.value);
+            }}
+          />
+        </div>
+      ))}
+      {feedback && <p>{feedback}</p>}
+    </div>
+  );
+};
