15206ba778d0bb95b83ac520f2585aa30af8ffb9
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2024-04-29 14:25:46 -0300

    implementa Alert Component

---
+++ src/components/Alert/Alert.tsx
@@ -0,0 +1,53 @@
+import { cva } from "class-variance-authority";
+import { cn } from "../../utils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTimes,
+  faCheckCircle,
+  faInfoCircle,
+  faExclamationTriangle,
+} from "@fortawesome/free-solid-svg-icons";
+
+const alertVariants = cva("feedback", {
+  variants: {
+    status: {
+      danger: "danger",
+      success: "success",
+      warning: "warning",
+      info: "info",
+    },
+  },
+});
+
+export type AlertProps = {
+  status: "danger" | "success" | "warning" | "info";
+  text: string;
+  className?: string;
+};
+
+const setIcon = (status: string) => {
+  switch (status) {
+    case "danger":
+      return faTimes;
+    case "success":
+      return faCheckCircle;
+    case "warning":
+      return faExclamationTriangle;
+    case "info":
+      return faInfoCircle;
+    default:
+      return faInfoCircle;
+  }
+};
+
+export const Alert = ({ text, status, className }: AlertProps) => {
+  const classes = cn(alertVariants({ status }), className);
+  const icon = setIcon(status);
+
+  return (
+    <span className={classes}>
+      <FontAwesomeIcon icon={icon} />
+      {text}
+    </span>
+  );
+};
