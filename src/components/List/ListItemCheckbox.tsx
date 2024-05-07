19048e9545b90197790571250d196cfbd4661007
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2024-05-07 14:22:47 -0300

    feat: Add ListItemCheckbox component to List module

---
+++ src/components/List/ListItemCheckbox.tsx
@@ -0,0 +1,18 @@
+import { Checkbox, CheckboxProps } from "../Checkbox";
+
+export type ListItemCheckboxProps = CheckboxProps & {
+  label?: string;
+  labeled?: boolean;
+};
+
+export const ListItemCheckbox = ({
+  id,
+  name,
+  label,
+  labeled = false,
+  ...props
+}: ListItemCheckboxProps) => {
+  return (
+    <Checkbox id={id} name={name} label={label} labeled={labeled} {...props} />
+  );
+};
