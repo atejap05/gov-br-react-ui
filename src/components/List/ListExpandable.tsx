d14e4e0047fd6c98f8882deb3c3bb199dcb6ef48
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2024-06-07 17:14:26 -0300

    feat: Add ListExpandable component for expandable lists

---
+++ src/components/List/ListExpandable.tsx
@@ -0,0 +1,54 @@
+import { useState, forwardRef } from "react";
+import { List, ListItem, ListItemText, ListItemIcon } from "./";
+import { ItemButton } from "../Item";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { Divider } from "../Divider";
+import { FontAwesomeIconProps } from "@fortawesome/react-fontawesome";
+
+export type ListExpandableProps = React.HTMLAttributes<HTMLUListElement> & {
+  className?: string;
+  label: string;
+  options: {
+    id: string;
+    icon?: FontAwesomeIconProps["icon"];
+    primaryText: string;
+    secondaryText?: string;
+  }[];
+};
+
+export const ListExpandable = forwardRef<HTMLUListElement, ListExpandableProps>(
+  ({ className, options, label }: ListExpandableProps, ref) => {
+    const [toggleIcon, setToggleIcon] = useState(faChevronDown);
+    const [expanded, setExpanded] = useState(false);
+
+    return (
+      <List ref={ref} className={className}>
+        <ItemButton
+          icon={toggleIcon}
+          onClick={() => {
+            setToggleIcon(
+              toggleIcon === faChevronDown ? faChevronUp : faChevronDown
+            );
+            setExpanded(!expanded);
+          }}
+        >
+          <ListItemText primary={label} />
+        </ItemButton>
+        <List className={expanded ? "" : "hidden"}>
+          {options.map(option => (
+            <div key={option.id}>
+              <ListItem>
+                {option.icon && <ListItemIcon icon={option.icon} />}
+                <ListItemText
+                  primary={option.primaryText}
+                  secondary={option.secondaryText}
+                />
+              </ListItem>
+              <Divider size="sm" />
+            </div>
+          ))}
+        </List>
+      </List>
+    );
+  }
+);
--- src/components/List/ListItem.tsx
+++ src/components/List/ListItem.tsx
@@ -13,16 +13,20 @@ const listItemVariants = cva("br-item", {
 export type ListItemProps = React.LiHTMLAttributes<HTMLLIElement> & {
   children: React.ReactNode;
   selected?: boolean;
+  onClick?: () => void;
   className?: string;
 };
 
 // TODO: feature: when checkbox is checked, add class selected
 
 export const ListItem = forwardRef<HTMLLIElement, ListItemProps>(
-  ({ children, selected, className, ...props }: ListItemProps, ref) => {
+  (
+    { children, selected, className, onClick, ...props }: ListItemProps,
+    ref
+  ) => {
     const classes = listItemVariants({ selected, className });
     return (
-      <li ref={ref} className={classes} {...props}>
+      <li ref={ref} className={classes} onClick={onClick} {...props}>
         <div className="row align-items-center">{children}</div>
       </li>
     );
