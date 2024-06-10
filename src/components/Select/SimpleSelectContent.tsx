08c9f6226901669349bd5b1a0701a746bfa5abca
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2024-06-10 11:21:37 -0300

    chore: Refactor SimpleSelectContent component to support defaultSelected prop

--- src/components/Select/SimpleSelectContent.tsx
+++ src/components/Select/SimpleSelectContent.tsx
@@ -4,57 +4,68 @@ import { useClickOutside } from "./hook/useClickOutside";
 export type SimpleSelectContentProps = {
   onSelectChange: (value: string) => void;
   onClickOutside: (value: boolean) => void;
+  defaultSelected?: string;
   options: {
-    value: string;
     label: string;
     id: string;
-    name: string;
-    defaultSelected?: boolean;
   }[];
 };
 
 export const SimpleSelectContent = forwardRef<
   HTMLInputElement,
   SimpleSelectContentProps
->(({ onSelectChange, onClickOutside, options, ...rest }, ref) => {
-  const [selectedItem, setSelectedItem] = useState("");
-  const brListRef = useRef<HTMLDivElement>(null);
+>(
+  (
+    { onSelectChange, onClickOutside, options, defaultSelected, ...rest },
+    ref
+  ) => {
+    const [selectedItem, setSelectedItem] = useState("");
+    const brListRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    onSelectChange(selectedItem);
-  }, [selectedItem, onSelectChange]);
+    useEffect(() => {
+      onSelectChange(selectedItem);
+    }, [selectedItem, onSelectChange]);
 
-  useClickOutside({ onClickOutside, elementRef: brListRef });
+    useClickOutside({ onClickOutside, elementRef: brListRef });
 
-  return (
-    <div ref={brListRef} className="br-list d-block" tabIndex={0}>
-      {options.map(option => (
-        <div
-          key={option.id}
-          className={
-            "br-item" +
-            (option.defaultSelected || selectedItem === option.label
-              ? " selected"
-              : "")
-          }
-          tabIndex={-1}
-          aria-selected={option.defaultSelected}
-          onClick={() => setSelectedItem(option.label)}
-        >
-          <div className="br-radio">
-            <input
-              ref={ref}
-              id={option.id}
-              type="radio"
-              name={option.name}
-              value={option.value}
-              defaultChecked={option.defaultSelected}
-              {...rest}
-            />
-            <label htmlFor={option.id}>{option.label}</label>
+    const isItemDefaultSelected = (option: { label: string; id: string }) => {
+      if (defaultSelected) {
+        return (
+          option.id === defaultSelected || option.label === defaultSelected
+        );
+      }
+      return false;
+    };
+
+    return (
+      <div ref={brListRef} className="br-list d-block" tabIndex={0}>
+        {options.map(option => (
+          <div
+            key={option.id}
+            className={
+              "br-item" +
+              (isItemDefaultSelected(option) || selectedItem === option.label
+                ? " selected"
+                : "")
+            }
+            tabIndex={-1}
+            aria-selected={isItemDefaultSelected(option)}
+            onClick={() => setSelectedItem(option.label)}
+          >
+            <div className="br-radio">
+              <input
+                ref={ref}
+                id={option.id}
+                type="radio"
+                value={option.label}
+                defaultChecked={isItemDefaultSelected(option)}
+                {...rest}
+              />
+              <label htmlFor={option.id}>{option.label}</label>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
-  );
-});
+        ))}
+      </div>
+    );
+  }
+);
