4860a519e7c3b2afa3325e50e5e21cf0b2c30247
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2024-07-08 16:57:15 -0300

    feat: Add Tag component and related components

---
+++ src/components/Tag/index.ts
@@ -0,0 +1,4 @@
+export { Tag, type TagProps } from "./Tag";
+export { TagStatus, type TagPropsStatus } from "./TagStatus";
+export { TagSelection, type TagSelectionProps } from "./TagSelection";
+export { TagCount, type TagCountProps } from "./TagCount";
