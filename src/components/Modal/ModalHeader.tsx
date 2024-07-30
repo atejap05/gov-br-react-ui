de98698000b2d188dac773d87f3e0e6fcc643b79
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2024-07-30 15:18:19 -0300

    feat: Add ModalHeader component for displaying the header of the modal dialog

---
+++ src/components/Modal/ModalHeader.tsx
@@ -0,0 +1,29 @@
+export type ModalHeaderProps = {
+  onClose: (close: boolean) => void;
+  title: string;
+  subtitle?: string;
+};
+
+const ModalHeader = ({ onClose, title, subtitle }: ModalHeaderProps) => {
+  return (
+    <div className={"br-modal-header"}>
+      <div className="d-flex flex-column">
+        <span className="text-up-02">{title}</span>
+        {subtitle && <span className="label text-regular">{subtitle}</span>}
+      </div>
+      <button
+        className="br-button close circle"
+        type="button"
+        data-dismiss="br-modal"
+        aria-label="Fechar"
+        onClick={() => onClose(true)}
+      >
+        <i className="fas fa-times" aria-hidden="true"></i>
+      </button>
+    </div>
+  );
+};
+
+ModalHeader.displayName = "ModalHeader";
+
+export { ModalHeader };
