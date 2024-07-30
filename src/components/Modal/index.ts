b21059e73c41a70654ffdd08e62a3c9109ee3a46
Joel Alves Pereira <joel.pereira@rfb.gov.br>
2024-07-30 15:17:46 -0300

    feat: Add Modal component and related types
    This commit adds the Modal component and its related types to the project. The Modal component provides a modal dialog for displaying content on top of the current page. It also includes the ModalBody and ModalFooter components for structuring the modal content. This addition will enable the use of modals in the application for various purposes such as displaying notifications, confirmation dialogs, or additional information.

---
+++ src/components/Modal/index.ts
@@ -0,0 +1,6 @@
+export { Modal } from "./Modal";
+export type { ModalProps } from "./Modal";
+export { ModalBody } from "./ModalBody";
+export type { ModalBodyProps } from "./ModalBody";
+export { ModalFooter } from "./ModalFooter";
+export type { ModalFooterProps } from "./ModalFooter";
